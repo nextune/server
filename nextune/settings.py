e22d335c3ff4ea7a9b31b69cb665b151d1b81cb8
RahulR100 <rahulrao@email.com>
2021-03-07 19:51:18 -0500

    first commit

--- nextune/settings.py
+++ nextune/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8m!tf(^lrw=-&boz)$yo4an52&l&4u*34x-w-hlt922a-f9t&k'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
